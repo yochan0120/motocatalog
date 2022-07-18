da17405fa45bb579edac6a711d3690348bd83cb3
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2022-07-18 15:49:32 +0900

    first commit

---
+++ src/main/java/jp/co/planaria/sample/motocatalog/MotocatalogApplication.java
@@ -0,0 +1,13 @@
+package jp.co.planaria.sample.motocatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotocatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotocatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/planaria/sample/motocatalog/MotocatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.planaria.sample.motocatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotocatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
