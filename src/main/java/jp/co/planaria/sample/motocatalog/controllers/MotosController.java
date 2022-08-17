7f7edeafe5c26c4f22232e4a90a733de53aad424
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2022-08-17 21:31:09 +0900

    ”動的ページを作った”

--- src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
+++ src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
@@ -1,9 +1,16 @@
 package jp.co.planaria.sample.motocatalog.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jp.co.planaria.sample.bean.Brand;
+import jp.co.planaria.sample.bean.Motorcycle;
 
 @Controller
 public class MotosController {
@@ -12,5 +19,28 @@ public class MotosController {
     public String hello(@RequestParam String name, Model model){
       model.addAttribute("name", name);
       return "test";
+    }
+
+    @GetMapping("/motos")
+    public String motos(Model model){
+      //ブランド
+      List<Brand> brands = new ArrayList<>();
+      brands.add(new Brand("01", "HONDA"));
+      brands.add(new Brand("02", "KAWASAKI"));
+      brands.add(new Brand("03", "YAMAHA"));
+      brands.add(new Brand("04", "SUZUKI"));
+
+      //バイク
+      List<Motorcycle> motos = new ArrayList<>();
+      motos.add(new Motorcycle(1, "GB350", 800, 1, "空冷", 500000, "いい音", new Brand("01", "HONDA"), 1, null, null));
+
+      motos.add(new Motorcycle(2, "Ninja", 800, 2, "水冷", 1000000, "すいすい", new Brand("02", "KAWASAKI"), 1, null, null));
+
+      motos.add(new Motorcycle(3, "Z900RS CAFE", 820, 4, "水冷", 1380000, "音めっちゃかっこいい", new Brand("02", "KAWASAKI"), 1, null, null));
+
+      model.addAttribute("brands", brands);  //インスタンスと同じ名前のキー名でmodelに情報をセットしている
+      model.addAttribute("motos", motos);
+
+      return "moto_list";
     }
   }
